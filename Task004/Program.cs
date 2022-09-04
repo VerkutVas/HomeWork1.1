c7ef86229e0111220e9507f9eaf6e2ed3ec8dd06
Vasiya <nva163@mail.ru>
2022-09-05 00:46:04 +0400

    На вход принемает число N

---
+++ Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число 1: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите число 2: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        for (int i = n; i <= m; i= i+1)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
