d9932bd6350fdec186b6bdaaab8077f33b438e2b
Vasiya <nva163@mail.ru>
2022-09-04 21:38:58 +0400

    Task001 a=5,b=7

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,16 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int a = 5, b = 7;
+
+int max = 7;
+int min = 0;
+
+if (a>max) max=a;
+if (b>max) max=b;
+
+Console.WriteLine("max =");
+Console.WriteLine(max);
+
+if (a<max) min= a;
+if (b<max) min= b;
+
+Console.WriteLine("min =");
+Console.WriteLine(min);
